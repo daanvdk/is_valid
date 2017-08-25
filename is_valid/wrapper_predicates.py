34015dbc34b2f4e44b104070bae8c3d1956d7e12
Daan van der Kallen <mail@daanvdk.com>
2017-08-25 23:44:29 +0200

    Add include keyword arg to is_tranformed

--- is_valid/wrapper_predicates.py
+++ is_valid/wrapper_predicates.py
@@ -4,14 +4,22 @@ import json
 def is_transformed(transform, predicate, *args, exceptions=[
     Exception
 ], msg='data can\'t be transformed', **kwargs):
-    def is_valid(data, explain=False):
+    def is_valid(data, explain=False, include=False):
         try:
             data = transform(data, *args, **kwargs)
         except Exception as e:
             if not any(isinstance(e, exc) for exc in exceptions):
                 raise e
-            return (False, msg) if explain else False
-        return predicate(data, explain=explain)
+            return (
+                (False, msg, None) if explain else (False, None)
+            ) if include else (
+                (False, msg) if explain else False
+            )
+        return ((
+            predicate(data, explain=True) + (data,)
+        ) if explain else (
+            predicate(data), data
+        )) if include else predicate(data, explain=explain)
     return is_valid
 
 
