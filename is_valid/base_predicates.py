6ad58976b46f9cabc2ef36442954b1e4948b507b
Daan van der Kallen <mail@daanvdk.com>
2017-08-27 21:08:06 +0200

    Added some docstrings

--- is_valid/base_predicates.py
+++ is_valid/base_predicates.py
@@ -1,8 +1,16 @@
 def is_fixed(valid, explanation):
+    """
+    Generates a predicate that returns a certain value for valid and
+    explanation that it will always return regardless of what data you put into
+    it.
+    """
     return lambda _, explain=False: (valid, explanation) if explain else valid
 
 
+#: A predicate that regardless of what data you put into it will always return
+#:
 is_something = is_fixed(True, 'data is something')
+#: is_nothing
 is_nothing = is_fixed(False, 'data is something')
 
 
