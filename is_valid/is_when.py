23419f66f24f311e8571ecb7d522f030cc3ea878
Daan van der Kallen <mail@daanvdk.com>
2019-11-08 16:25:53 +0100

    Remove unnecessary pred arg from is_when

--- is_valid/is_when.py
+++ is_valid/is_when.py
@@ -6,15 +6,13 @@ from .is_nothing import is_nothing
 
 class is_when(Predicate):
 
-    def __init__(self, value, pred=True, then=is_something, else_=is_nothing):
+    def __init__(self, value, then=is_something, else_=is_nothing):
         self._value = value
-        self._pred = to_pred(pred)
         self._then = to_pred(then)
         self._else = to_pred(else_)
 
     def _evaluate(self, data, explain, context):
-        value = context(self._value)
-        if self._pred(value, False, context):
+        if context(self._value):
             return self._then(data, explain, context)
         else:
             return self._else(data, explain, context)
